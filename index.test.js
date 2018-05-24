3a0f7ea22bf6d0105c2d18e0a60bb0e07eaaa38b
vamsiampolu <vamsiampolu3000@gmail.com>
2018-05-24 13:58:53 +1000

    Adds jest, babel-plugin, adds two additional test cases

--- index.test.js
+++ index.test.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import Enzyme, { mount, shallow } from 'enzyme';
+/* eslint-env node, jest */
 
-import Adapter from 'enzyme-adapter-react-15.4';
-Enzyme.configure({ adapter: new Adapter() });
+import React from 'react'
+import Enzyme, {mount, shallow} from 'enzyme'
 
-const Hello = props => {
-  return <p>Hello World</p>;
-};
+import Adapter from 'enzyme-adapter-react-15.4'
+Enzyme.configure({adapter: new Adapter()})
+
+function Hello (props) {
+  return <p>Hello World</p>
+}
+
+class HelloClass extends React.Component {
+  render () {
+    return <p>Hello World</p>
+  }
+}
 
 it('should mount the test', () => {
-  let wrapper = mount(<Hello />);
+  let wrapper = mount(<Hello />)
 
-  expect(true).toBe(true);
-});
+  expect(true).toBe(true)
+})
 
+describe('Shallow rendering composite nodes', () => {
+  it('should shallow render a Stateless Function Component', () => {
+    let wrapper = shallow(<Hello />)
+    expect(true).toBe(true)
+  })
 
-it('should shallow render the test', () => {
-  let wrapper = shallow(<Hello />);
+  it('should shallow render a class component', () => {
+    let wrapper = shallow(<HelloClass />)
 
-  expect(true).toBe(true);
-});
+    expect(true).toBe(true)
+  })
+})
+
+describe('Shallow render known node types', () => {
+  it('should render a paragraph tag', () => {
+    let wrapper = shallow(<p>Hello</p>)
+
+    expect(true).toBe(true)
+  })
+})
